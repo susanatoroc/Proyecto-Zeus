8f61614c9c1fad9f6ff452473bc5784654a8b8c1
39783211 <dianaiselaospina@gmail.com>
2022-10-22 20:49:29 -0500

    Agregar entidad persona

---
+++ src/models/persona.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Persona extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombre: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  Fecha_nacimiento: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Persona>) {
+    super(data);
+  }
+}
+
+export interface PersonaRelations {
+  // describe navigational properties here
+}
+
+export type PersonaWithRelations = Persona & PersonaRelations;
