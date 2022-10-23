61957ae999155f536310c7f5acb3230b62501b01
susanatoroc <susana.toro.contreras@gmail.com>
2022-10-22 20:27:36 -0500

    Backend Mantimiento TIC Entidad Empresa

---
+++ BackendMantenimientoTIC/src/models/empresa.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Empresa extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  CIF: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Empresa>) {
+    super(data);
+  }
+}
+
+export interface EmpresaRelations {
+  // describe navigational properties here
+}
+
+export type EmpresaWithRelations = Empresa & EmpresaRelations;
